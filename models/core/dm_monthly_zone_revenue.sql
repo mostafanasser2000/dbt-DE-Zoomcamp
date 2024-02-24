bdced8162dc42087df21c5a3c4b32aafacfa06f6
mostafa nasser <mustafanasser503@gmail.com>
2024-02-24 17:49:21 +0000

    add models - stg_green_tripdata - stg_yellow_tripdata - dim_zones - fact_trips - dm_monthly_revenue
    add seeds
    - taxi_zone_lookup

---
+++ models/core/dm_monthly_zone_revenue.sql
@@ -0,0 +1,27 @@
+{{ config(materialized="table") }}
+
+with trips_data as (select * from {{ ref("fact_trips") }})
+
+select
+    -- Reveneue grouping
+    pickup_zone as reveneue_zone,
+    {{ dbt.date_trunc("month", "pickup_datetime") }} as reveneue_month,
+    service_type,
+
+    -- Revenue calculations
+    sum(fare_amount) as revenue_monthly_fare,
+    sum(extra) as revenue_monthly_extra,
+    sum(mta_tax) as revenue_monthly_mta_tax,
+    sum(tip_amount) as revenue_monthly_tip_amount,
+    sum(tolls_amount) as revenue_monthly_tolls_amount,
+    sum(ehail_fee) as revenue_monthly_ehail_fee,
+    sum(improvement_surcharge) as revenue_monthly_improvement_surcharge,
+    sum(total_amount) as revenue_monthly_total_amount,
+
+    -- Additional calculations
+    count(tripid) as total_monthly_trips,
+    avg(passenger_count) as avg_monthly_passenger_count,
+    avg(trip_distance) as avg_monthly_trip_distance
+
+from trips_data
+group by 1, 2, 3
